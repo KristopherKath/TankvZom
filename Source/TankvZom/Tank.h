a857d513bdec98bfb9b89848432c5f4d89c83d97
Kris <kristopher.kath@gmail.com>
2020-07-10 19:46:08 -0700

    Code Edits

---
+++ Source/TankvZom/Tank.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+//#include "Components/ArrowComponent.h"
+//#include "PaperSpriteComponent.h"
+#include "Tank.generated.h"
+
+class UArrowComponent;
+class UPaperSpriteComponent;
+
+UCLASS()
+class TANKVZOM_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATank();
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	// Helpful debug tool - which way the tank is facing
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UArrowComponent* TankDirection;
+
+	// Sprite for the tank body.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	UPaperSpriteComponent* TankSprite;
+};
